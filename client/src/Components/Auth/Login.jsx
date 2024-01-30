31d4e26103e77da2ba725503e5bb9eb6718e0c39
mohnishgorana1 <mohnishgorana1@gmail.com>
2024-01-30 18:51:23 +0530

    login functionality done

--- client/src/Components/Auth/Login.jsx
+++ client/src/Components/Auth/Login.jsx
@@ -1,16 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { login } from "../../Redux/authSlice";
 
 function Login() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    
+  // const user = useSelector((state) => state.auth.user);
+
+  const [myUser, setMyUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setMyUser({
+      ...myUser,
+      [name]: value,
+    });
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("/api/user/login", myUser);
+      console.log(response);
+      console.log(myUser);
+
+      if (response.status === 200) {
+        // user = response?.data;
+        dispatch(login(response?.data));
+        console.log("Login Successful");
+
+        // navigate('/chats')
+      } else {
+        console.log("Login failed");
+      }
+    } catch (error) {
+      console.log("Error login", error.message);
+    }
+
+    setMyUser({
+      email: "",
+      password: "",
+    });
+  };
+
   return (
     <div className="w-full sm:px-5 ">
-      <form className="flex flex-col gap-y-4 items-center justify-center pt-2">
+      <form
+        className="flex flex-col gap-y-4 items-center justify-center pt-2"
+        onSubmit={handleLogin}
+      >
         <div className="flex items-center justify-between sm:justify-none w-full gap-x-2">
           <label htmlFor="">Email</label>
           <input
             type="text"
             placeholder="Enter Email"
+            name="email"
+            value={myUser.email}
+            onChange={handleFormChange}
             className="text-center border bg-transparent w-[65%] py-1"
           />
         </div>
@@ -19,6 +72,9 @@ function Login() {
           <input
             type="text"
             placeholder="Enter Password"
+            name="password"
+            value={myUser.password}
+            onChange={handleFormChange}
             className="text-center border bg-transparent w-[65%] py-1"
           />
         </div>

335c5bd3562436d10c6e89f492eeee3caa69ee4b
mohnishgorana1 <mohnishgorana1@gmail.com>
2024-01-30 11:57:15 +0530

    db connection init

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
 import app from "./app.js";
-// import connectToDB from './config/dbConnection.js'
+import connectToDB from "./config/dbConnection.js";
 // import { v2 } from "cloudinary";
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, async () => {
-  // await connectToDB();
+  await connectToDB();
   console.log(`SERVER UP At http://localhost:${PORT}`);
 });

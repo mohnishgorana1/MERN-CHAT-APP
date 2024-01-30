266ce5ca904d5c25fd593714e5b60bd8cd16057c
mohnishgorana1 <mohnishgorana1@gmail.com>
2024-01-30 11:46:16 +0530

    project setup init

---
+++ server/src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+
+const app = express();
+
+// middlewares:  express.json | cors | cookieParser | morgan |
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+// default route
+app.use("/ping", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "PONG",
+  });
+});
+
+export default app;
